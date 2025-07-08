484b96f57a3e62667232e6cae30e9d880d44a70f
jpgaviola <jpgaviola@semirarampc.com>
2025-07-08 15:40:31 +0800

    Fix the toast success and error message in login page

--- obj/Debug/net9.0/HelpdeskBlazor.AssemblyInfo.cs
+++ obj/Debug/net9.0/HelpdeskBlazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelpdeskBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fe74d456255cb53244dc93e8c0ae66ce0286f1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76099839501f64f5e4af92f3dd81a641e5372d56")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelpdeskBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelpdeskBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
