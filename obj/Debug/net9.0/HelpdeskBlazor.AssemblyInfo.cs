880269ab171ab749ce91b3ad5736672aa9249158
jpgaviola <jpgaviola@semirarampc.com>
2025-07-08 15:00:33 +0800

    Add page title to all features

--- obj/Debug/net9.0/HelpdeskBlazor.AssemblyInfo.cs
+++ obj/Debug/net9.0/HelpdeskBlazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelpdeskBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7a0c301e5151f870320db0fb9c7aeceeaca0a24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb4c7470ad194d293328cd4d1a18d1d5bc0de974")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelpdeskBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelpdeskBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
